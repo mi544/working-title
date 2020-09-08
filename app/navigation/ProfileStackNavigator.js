ae8da9f546812de5473ebc46851984c81f7e4eaa
Maksim Verkhoturov <sd32@protonmail.com>
2020-09-08 07:43:55 -0600

    created a profile stack

---
+++ app/navigation/ProfileStackNavigator.js
@@ -0,0 +1,10 @@
+import { createStackNavigator } from "react-navigation-stack";
+
+import ProfileScreen from "../screens/ProfileScreen";
+
+const ProfileStackNavigator = createStackNavigator({
+    Profile: ProfileScreen
+});
+
+
+export default ProfileStackNavigator;
