4f2f0983eb24b5b04ec0205c74bdaca4ed47bf6d
Maksim Verkhoturov <sd32@protonmail.com>
2020-09-08 09:51:08 -0600

    making the loading spinner a component

---
+++ app/components/Loading.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Provider as PaperProvider, ActivityIndicator, Colors } from "react-native-paper";
+
+import theme from "../constants/theme";
+
+const Loading = props => {
+    return (
+        <PaperProvider theme={theme}>
+            <ActivityIndicator
+                animating
+                color={Colors.red800}
+                size="large"
+            />
+        </PaperProvider>
+    );
+};
+
+export default Loading; 
