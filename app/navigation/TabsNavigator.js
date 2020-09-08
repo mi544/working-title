c9bd9af03f9c607d6e12a3f68245e08bd812eb7c
Maksim Verkhoturov <sd32@protonmail.com>
2020-09-08 07:42:39 -0600

    added device check and improved bottom tabs for android

--- app/navigation/TabsNavigator.js
+++ app/navigation/TabsNavigator.js
@@ -1,12 +1,46 @@
+import React from "react";
+import { Platform } from "react-native";
 import { createBottomTabNavigator } from "react-navigation-tabs";
+import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs"
+
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import ExampleStackNavigator from "./ExampleStackNavigator";
-import ProfileStack from "./ProfileStack";
+import FriendsStackNavigator from "./FriendsStackNavigator";
 
-const TabsNavigator = createBottomTabNavigator({
-    Foo: ExampleStackNavigator,
-    Bar: ProfileStack
-});
+const tabScreenConfig = {
+    Foo: {
+        screen: ExampleStackNavigator,
+        navigationOptions: {
 
+            tabBarIcon: tabInfo => (
+                <MaterialCommunityIcons
+                    name="home-circle"
+                    size={26}
+                />
+            )
+        }
+    },
+    Friends: {
+        screen: FriendsStackNavigator,
+        navigationOptions: {
+
+            tabBarIcon: tabInfo => (
+                <MaterialCommunityIcons
+                    name="account-multiple"
+                    size={26}
+                />
+            )
+        }
+    },
+};
+
+const TabsNavigator = Platform.OS === "android"
+    ? createMaterialBottomTabNavigator(tabScreenConfig, {
+        shifting: false,
+        labeled: false,
+        barStyle: { backgroundColor: "white" }
+    })
+    : createBottomTabNavigator(tabScreenConfig);
 
 export default TabsNavigator;
