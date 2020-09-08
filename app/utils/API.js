08326a1ef0eac805bef7ff2612d66cbbb0673085
Maksim Verkhoturov <sd32@protonmail.com>
2020-09-08 07:45:01 -0600

    created a controller for all the API calls to the server

---
+++ app/utils/API.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+import { SERVER } from "@env";
+
+export default {
+    findAllUsers: () => axios.get(`${SERVER}/api/users`),
+    findUserById: (id) => axios.get(`${SERVER}/api/users/${id}`)
+};
