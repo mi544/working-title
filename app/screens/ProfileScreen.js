4a4cdd7f7be2c6dcae9c3d85f2604480c9ca4dc6
Maksim Verkhoturov <sd32@protonmail.com>
2020-09-25 22:57:05 -0600

    removed unnecessary console logs

--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -18,19 +18,16 @@ import ProfileCard from "../components/Profile/ProfileCard";
 const ProfileScreen = props => {
     const [isLoaded, setIsLoaded] = useState(false);
     const [profileInfo, setProfileInfo] = useState();
-    console.log(props);
 
     useEffect(() => {
         loadProfileInfo();
     }, []);
 
     const loadProfileInfo = () => {
-        console.log("param", props.navigation.getParam("userId"));
         API.findUserById(props.navigation.getParam("userId"))
             .then(({ data }) => {
                 setProfileInfo(data);
                 setIsLoaded(true);
-                console.log(profileInfo);
             })
             .catch(err => console.log(err));
     };
