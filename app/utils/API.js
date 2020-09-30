6e210804b1295f0157d9f952272f3825a443fdef
Maksim Verkhoturov <sd32@protonmail.com>
2020-09-29 22:03:20 -0600

    using fetch instead of axios

--- app/utils/API.js
+++ app/utils/API.js
@@ -1,7 +1,7 @@
-import axios from "axios";
+// import axios from "axios";
 import { SERVER } from "@env";
 
 export default {
-    findAllUsers: () => axios.get(`${SERVER}/api/users`),
-    findUserById: id => axios.get(`${SERVER}/api/users/${id}`)
+    findAllFriends: () => fetch(`${SERVER}/api/users`),
+    findUserById: id => fetch(`${SERVER}/api/users/${id}`)
 };
