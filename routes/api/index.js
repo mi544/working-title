6db1f9e1cfe5616e92d061f29e008810345a3e61
Maksim Verkhoturov <sd32@protonmail.com>
2020-09-30 17:22:32 -0600

    better api paths

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 
+router.use("/profile", require("./profile"));
+router.use("/friends", require("./friends"));
 router.use("/users", require("./users"));
 
 module.exports = router;
