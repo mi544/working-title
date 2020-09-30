38b98e4bb2aa626bb88106ad2517f6645d2d03ae
Maksim Verkhoturov <sd32@protonmail.com>
2020-09-29 22:01:22 -0600

    adding synchronous actions for 3 different states on fetching friends

---
+++ app/store/actions/friends.js
@@ -0,0 +1,15 @@
+export const SHOW_FRIENDS = "SHOW_FRIENDS";
+export const RECEIVE_FRIENDS = "RECEIVE_FRIENDS";
+export const ERROR_FRIENDS = "ERROR_FRIENDS";
+
+export const showFriends = () => {
+    return { type: SHOW_FRIENDS };
+};
+
+export const errorFriends = () => {
+    return { type: ERROR_FRIENDS };
+};
+
+export const receiveFriends = friends => {
+    return { type: RECEIVE_FRIENDS, friends: friends };
+};
