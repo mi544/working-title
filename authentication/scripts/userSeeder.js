31d758b74230ab638fcdb81589c44d6013e06589
Maksim Verkhoturov <sd32@protonmail.com>
2020-10-05 10:29:09 -0600

    change seeder data for users to have UUID of proper format

--- authentication/scripts/userSeeder.js
+++ authentication/scripts/userSeeder.js
@@ -2,9 +2,21 @@ const db = require("../models");
 const { exit } = require("process");
 
 const dataToSeed = [
-    { userId: "wf32khsdps3", email: "example@email.com", password: "12gmj3kd@tghfds" },
-    { userId: "df23ths23", email: "now@email.com", password: "dg5943E3%23h" },
-    { userId: "g320t50ew1", email: "later@email.com", password: "dgh43io)@tyrgdS" }
+    {
+        userId: "27b891d7-0cd8-43a2-b4da-8f0b0f833223",
+        email: "example@email.com",
+        password: "12gmj3kd@tghfds"
+    },
+    {
+        userId: "bc204d45-96f7-48b4-8c16-f04a341ef627",
+        email: "now@email.com",
+        password: "dg5943E3%23h"
+    },
+    {
+        userId: "6f1397e6-06e9-4d6b-9fef-f4928826eb82",
+        email: "later@email.com",
+        password: "dgh43io)@tyrgdS"
+    }
 ];
 db.sequelize
     .sync({ force: true })
