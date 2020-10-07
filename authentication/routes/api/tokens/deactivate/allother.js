83ad0c61c0eed2026114bf47f2972bb05dbbec51
Maksim Verkhoturov <sd32@protonmail.com>
2020-10-07 00:23:57 -0600

    logic for deactivating all sessions other than this one

--- authentication/routes/api/tokens/deactivate/allother.js
+++ authentication/routes/api/tokens/deactivate/allother.js
@@ -1,8 +1,28 @@
 const router = require("express").Router();
+const controller = require("../../../../controllers");
 
-router.route("/").get((req, res) => {
-    // get a token and a userId
-    // deactivate all tokens for this user but this token
+router.route("/").put(async (req, res) => {
+    try {
+        if (!req.body.userId) {
+            return res.send({ success: false, reason: "No userId provided." });
+        }
+        if (!req.body.activeToken) {
+            return res.send({ success: false, reason: "No token provided." });
+        }
+
+        const deactivationResult = await controller.tokenController.deactivateAllTokensOfUserBesidesActive(
+            req.body.userId,
+            req.body.activeToken
+        );
+
+        if (!deactivationResult.success) {
+            return res.send({ success: false, reason: deactivationResult.reason });
+        }
+
+        return res.send({ success: true, token: deactivationResult.token });
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 module.exports = router;
