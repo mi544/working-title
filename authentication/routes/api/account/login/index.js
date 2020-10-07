7e451d4581fde830e2124d0fac8ed54e98cb923f
Maksim Verkhoturov <sd32@protonmail.com>
2020-10-07 00:10:32 -0600

    logic to handle logging in

--- authentication/routes/api/account/login/index.js
+++ authentication/routes/api/account/login/index.js
@@ -1,10 +1,36 @@
 const router = require("express").Router();
+const controller = require("../../../../controllers");
 
-router.route("/").get((req, res) => {
-    // get an email, password
-    // compare against the db
-    // give token back if successful
-    // give false with reason if unsuccessful
+router.route("/").get(async (req, res) => {
+    try {
+        if (!req.body.email) {
+            return res.send({ success: false, reason: "No email provided." });
+        }
+        if (!req.body.password) {
+            return res.send({ success: false, reason: "No password provided." });
+        }
+
+        const validationResult = await controller.userController.validateUserCredentials(
+            req.body.email,
+            req.body.password
+        );
+
+        if (!validationResult.success) {
+            return res.send({ success: false, reason: validationResult.reason });
+        }
+
+        const tokenResult = await controller.tokenController.createAndAssignTokenToUser(
+            validationResult.userId
+        );
+
+        if (!tokenResult.success) {
+            return res.send({ success: false, reason: creationResult.reason });
+        }
+
+        return res.send({ success: true, token: tokenResult.token });
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 module.exports = router;
