44c1abc9c03a984f0b1aa32a5f150a7357d2c3df
Maksim Verkhoturov <sd32@protonmail.com>
2020-10-06 20:37:15 -0600

    user controller with validation

--- authentication/controllers/userController.js
+++ authentication/controllers/userController.js
@@ -1,62 +1,72 @@
 const db = require("../models");
-const { v4: uuidv4 } = require("uuid");
+const idGen = require("../utils/randomIdGenerator");
 
 module.exports = {
+    /**
+     * @typedef CallResult
+     * @type {object}
+     * @property {boolean} success - Specifying whether the call was successful
+     * @property {string} reason - Providing a reason if it wasn't successful
+     * @property {string} userId - userId of the user. Only included if the call was successful
+     */
+
     /** Creates a user with provided credentials
      *
      * Function is ASYNC! AWAIT it!
      * @param {string} email - The email of account to be created
      * @param {string} password - The password of account to be created
-     * @returns {string} Created user's userId
+     * @returns {CallResult} Object with information about the result of creation
      */
     createUserWithCredentials: async (email, password) => {
-        /** Generates a unique UUID
+        /** Generates a unique ID
          *
          * Function is ASYNC! AWAIT it!
-         * @returns {string} Unique UUID
+         * @returns {string} Unique ID
          */
-        const generateUUID = async () => {
-            let queryResult;
-            let currentUUID;
+        const generateUniqueId = async () => {
             try {
+                let queryResult;
+                let currentId;
                 do {
-                    currentUUID = uuidv4();
-                    queryResult = await db.User.findAll({
-                        where: { userId: currentUUID }
+                    currentId = idGen.generateId(10);
+                    queryResult = await db.User.findOne({
+                        where: { userId: currentId }
                     });
-                } while (queryResult.length);
+                } while (queryResult !== null);
+                return currentId;
             } catch (error) {
                 console.log(error);
             }
-            return currentUUID;
         };
         try {
-            const UUID = await generateUUID();
+            const queryEmail = await db.User.findOne({
+                where: { email: email }
+            });
+            if (queryEmail !== null) {
+                return { success: false, reason: "Email already exists." };
+            }
+
+            const uniqueId = await generateUniqueId();
             const creationResult = await db.User.create({
-                userId: UUID,
+                userId: uniqueId,
                 email: email,
                 password: password
             });
-            return await creationResult.toJSON().userId;
+            return {
+                success: true,
+                userId: await creationResult.toJSON().userId
+            };
         } catch (error) {
             console.log(error);
         }
     },
-    /**
-     * @typedef ValidationResults
-     * @type {object}
-     * @property {boolean} success - Specifying whether the validation was successful
-     * @property {string} reason - Providing a reason if it wasn't successful
-     * @property {string} userId - userId of the validated user. Only included if validation was successful
-     */
-
     /**
      * Validates provided user credentials
      *
      * Function is ASYNC! AWAIT it!
      * @param {string} email - The email of account to be validated
      * @param {string} password - The password of account to be validated
-     * @returns {ValidationResults} Object with information about the result of validation
+     * @returns {CallResult} Object with information about the result of validation
      */
     validateUserCredentials: async (email, password) => {
         try {
