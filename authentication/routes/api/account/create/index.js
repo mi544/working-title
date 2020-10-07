9f969caee8f588337c5156e4371c57dd5feae216
Maksim Verkhoturov <sd32@protonmail.com>
2020-10-07 00:03:30 -0600

    basic logic for creating an account

--- authentication/routes/api/account/create/index.js
+++ authentication/routes/api/account/create/index.js
@@ -1,9 +1,35 @@
 const router = require("express").Router();
+const controller = require("../../../../controllers");
 
-router.route("/").get((req, res) => {
-    // email, password
-    // create account
-    // return token
+router.route("/").post(async (req, res) => {
+    try {
+        if (!req.body.email) {
+            return res.send({ success: false, reason: "No email provided." });
+        }
+        if (!req.body.password) {
+            return res.send({ success: false, reason: "No password provided." });
+        }
+        const creationResult = await controller.userController.createUserWithCredentials(
+            req.body.email,
+            req.body.password
+        );
+
+        if (!creationResult.success) {
+            return res.send({ success: false, reason: creationResult.reason });
+        }
+
+        const tokenResult = await controller.tokenController.createAndAssignTokenToUser(
+            creationResult.userId
+        );
+
+        if (!tokenResult.success) {
+            return res.send({ success: false, reason: creationResult.reason });
+        }
+
+        return res.send({ success: true, token: tokenResult.token });
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 module.exports = router;
