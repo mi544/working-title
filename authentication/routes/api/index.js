11fdaf580bce55e0c9a89e229d8b104d72fc1d69
Maksim Verkhoturov <sd32@protonmail.com>
2020-10-07 15:24:54 -0600

    fixing routes

--- authentication/routes/api/index.js
+++ authentication/routes/api/index.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 
-router.use("/signup", require("./signup"));
-router.use("/singin", require("./singin"));
+router.use("/account", require("./account"));
 router.use("/tokens", require("./tokens"));
 
 module.exports = router;
