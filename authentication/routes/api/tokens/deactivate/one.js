01baaaee37e507fc783731e721a0092f71ff1a85
Maksim Verkhoturov <sd32@protonmail.com>
2020-10-07 15:21:18 -0600

    basic logic for deactivating one token

--- authentication/routes/api/tokens/deactivate/one.js
+++ authentication/routes/api/tokens/deactivate/one.js
@@ -1,9 +1,24 @@
 const router = require("express").Router();
+const controller = require("../../../../controllers");
 
-router.route("/").get((req, res) => {
-    // get a token
-    // deactivate this token
-    // send back true
+router.route("/").put(async (req, res) => {
+    try {
+        if (!req.body.token) {
+            return res.send({ success: false, reason: "No token provided." });
+        }
+
+        const deactivationResult = await controller.tokenController.deactivateTokenOfUser(
+            req.body.token
+        );
+
+        if (!deactivationResult.success) {
+            return res.send({ success: false, reason: deactivationResult.reason });
+        }
+
+        return res.send({ success: true, token: deactivationResult.token });
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 module.exports = router;
