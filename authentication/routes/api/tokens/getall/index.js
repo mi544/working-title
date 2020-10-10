d7a7ef309725d9688d005f8a6917163d5d431768
Maksim Verkhoturov <sd32@protonmail.com>
2020-10-09 18:17:40 -0600

    adding getall route to show all user tokens

---
+++ authentication/routes/api/tokens/getall/index.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+const controller = require("../../../../controllers");
+
+router.route("/").put(async (req, res) => {
+    try {
+        if (!req.body.userId) {
+            return res.send({ success: false, reason: "No userId provided." });
+        }
+        if (!req.body.activeToken) {
+            return res.send({ success: false, reason: "No token provided." });
+        }
+
+        const allTokens = await controller.tokenController.getAllTokensAssociatedWithUserId(
+            req.body.userId
+        );
+
+        if (!allTokens.success) {
+            return res.send({ success: false, reason: allTokens.reason });
+        }
+
+        return res.send({ success: true, token: allTokens.token });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+module.exports = router;
--- authentication/routes/api/tokens/index.js
+++ authentication/routes/api/tokens/index.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 
 router.use("/deactivate", require("./deactivate"));
 router.use("/verify", require("./verify"));
+router.use("/getall", require("./getall"));
 
 module.exports = router;
