5a4212faae214334d1b64c3bfa12382ef7bb253e
Maksim Verkhoturov <sd32@protonmail.com>
2020-10-09 19:04:10 -0600

    improved seeders script, now uses the same controllers used in paths

--- authentication/scripts/userSeeder.js
+++ authentication/scripts/userSeeder.js
@@ -1,29 +1,32 @@
 const db = require("../models");
 const { exit } = require("process");
+const controller = require("../controllers");
 
 const dataToSeed = [
     {
-        userId: "27b891d7-0cd8-43a2-b4da-8f0b0f833223",
         email: "example@email.com",
         password: "12gmj3kd@tghfds"
     },
     {
-        userId: "bc204d45-96f7-48b4-8c16-f04a341ef627",
         email: "now@email.com",
         password: "dg5943E3%23h"
     },
     {
-        userId: "6f1397e6-06e9-4d6b-9fef-f4928826eb82",
         email: "later@email.com",
         password: "dgh43io)@tyrgdS"
     }
 ];
-db.sequelize
-    .sync({ force: true })
-    .then(() => {
-        db.User.bulkCreate(dataToSeed, { validate: true }).then(() => {
-            console.log("Successfully added the users!");
-            exit();
-        });
-    })
-    .catch(err => console.log(err));
+db.sequelize.sync({ force: true }).then(async () => {
+    try {
+        for (const item of dataToSeed) {
+            await controller.userController.createUserWithCredentials(
+                item.email,
+                item.password
+            );
+        }
+        exit();
+    } catch (error) {
+        console.log(error);
+        exit();
+    }
+});
